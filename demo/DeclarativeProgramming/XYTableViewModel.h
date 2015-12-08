1306002c2909bb3107c164a305b811878e7aa7fe
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2015-12-08 14:42:30 +0800

    add xytableviewmodel class

---
+++ demo/DeclarativeProgramming/XYTableViewModel.h
@@ -0,0 +1,13 @@
+//
+//  XYTableViewModel.h
+//  Demo
+//
+//  Created by hzyuxiaohua on 15/12/8.
+//  Copyright © 2015年 X Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYTableViewModel : NSObject
+
+@end
---
+++ demo/DeclarativeProgramming/XYTableViewModel.m
@@ -0,0 +1,13 @@
+//
+//  XYTableViewModel.m
+//  Demo
+//
+//  Created by hzyuxiaohua on 15/12/8.
+//  Copyright © 2015年 X Co., Ltd. All rights reserved.
+//
+
+#import "XYTableViewModel.h"
+
+@implementation XYTableViewModel
+
+@end
